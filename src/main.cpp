5aacbeb52e42af39675f1cdf870832b998f5b894
Will S <sc19ws@leeds.ac.uk>
2022-04-20 18:23:29 +0100

    Removed requirement for serial connection

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,6 @@ int catchup = 0;
 void setup() {
     // Open serial communications and wait for port to open:
     Serial.begin(9600);
-    while (!Serial) {
-        ;  // wait for serial port to connect. Needed for native USB port only
-    }
 
     if(!IMU.begin()){
 		Serial.print("IMU init failed");
@@ -53,12 +50,6 @@ void loop() {
     //file handling
     if(record){
         saveData();
-        // ledCount++;
-        // if(ledCount>20){
-        //     led = !led;
-        //     ledCount = 0;
-        //     digitalWrite(LED_BUILTIN, led);
-        // }
         if(pressed){
             closeFile();
             record = false;
